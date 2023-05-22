ae92f6d8f58955789588f6834b7ff564131771ab
Lesiba Moekwa <lesiba.moekwa@dadevs.co.za>
2023-05-22 10:01:53 +0200

    Initial Commit

---
+++ DiceGame1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame1
+//
+//  Created by DA MAC M1 139 on 2023/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
