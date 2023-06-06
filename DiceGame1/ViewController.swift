5e8d9c1b909d6517475e94d03ea179f3aa1720f5
Lesiba Moekwa <lesiba.moekwa@dadevs.co.za>
2023-06-06 07:59:07 +0200

    Simple dice game using swift

--- DiceGame1/ViewController.swift
+++ DiceGame1/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Dice2: UIImageView!
+    @IBOutlet weak var Dice1: UIImageView!
+    
+    @IBOutlet weak var lblDice: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnRoll(_ sender: Any){
+        
+        
+       // let randomInt2 = Int.random(in: 1..<6)
+        //Dice2.image = UIImage(named: String(randomInt2))
+        
+        var c = 0
+        var varTimer = Timer.scheduledTimer(withTimeInterval: 0.08, repeats: true)
+        { (varTimer) in
+            c=c+1
+            
+            let randomInt1 = Int.random(in: 1..<7)
+            self.Dice1.image = UIImage(named: String(randomInt1))
+            
+            let randomInt2 = Int.random(in: 1..<7)
+            self.Dice2.image = UIImage(named: String(randomInt2))
+            
+            var total = randomInt1 + randomInt2
+            if((randomInt1 == 6) && randomInt2 == 6)
+            {
+                self.lblDice.text = "WINNER"
+            }
+            else
+            {
+                self.lblDice.text = "TRY AGAIN"
+            }
+            
+            
+            if(c == 20)
+            {
+                varTimer.invalidate()
+            }
+        }
+    }
+    
+    
 }
 
